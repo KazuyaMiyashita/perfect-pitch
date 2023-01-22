d73aebede71331072d20cfa1f41115d7d98a1063
KazuyaMiayshita <miy1924@gmail.com>
2023-01-22 09:39:41 +0900

    20230122.001 音がちゃんとなる

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,10 @@
-import {OpenSheetMusicDisplay as OSMD } from 'opensheetmusicdisplay'
+import {OpenSheetMusicDisplay as OSMD} from 'opensheetmusicdisplay'
 import MidiPlayer from 'midi-player-js';
-import Soundfont, {Player} from 'soundfont-player';
+import Soundfont, {InstrumentName} from "soundfont-player";
 import {transposeMusicXML} from './transpose'
 
 const init = () => {
   console.log('init')
-
-  const sheets = [
-    './sheets/chouon-001-four-staves.musicxml',
-    './sheets/chouon-001-grand-staff.musicxml',
-    './sheets/chouon-002-four-staves.musicxml',
-    './sheets/chouon-002-grand-staff.musicxml',
-    './sheets/chouon-003-four-staves.musicxml',
-    './sheets/chouon-003-grand-staff.musicxml',
-    './sheets/chouon-004-four-staves.musicxml',
-    './sheets/chouon-004-grand-staff.musicxml',
-    './sheets/chouon-005-four-staves.musicxml',
-    './sheets/chouon-005-grand-staff.musicxml',
-    './sheets/chouon-006-four-staves.musicxml',
-    './sheets/chouon-006-grand-staff.musicxml',
-    './sheets/chouon-007-four-staves.musicxml',
-    './sheets/chouon-007-grand-staff.musicxml',
-    './sheets/chouon-008-four-staves.musicxml',
-    './sheets/chouon-008-grand-staff.musicxml',
-    './sheets/chouon-009-four-staves.musicxml',
-    './sheets/chouon-009-grand-staff.musicxml',
-    './sheets/chouon-010-four-staves.musicxml',
-    './sheets/chouon-010-grand-staff.musicxml',
-    './sheets/chouon-011-four-staves.musicxml',
-    './sheets/chouon-011-grand-staff.musicxml',
-    './sheets/chouon-012-four-staves.musicxml',
-    './sheets/chouon-012-grand-staff.musicxml',
-    './sheets/chouon-013-four-staves.musicxml',
-    './sheets/chouon-013-grand-staff.musicxml',
-    './sheets/chouon-014-four-staves.musicxml',
-    './sheets/chouon-014-grand-staff.musicxml',
-    './sheets/chouon-015-four-staves.musicxml',
-    './sheets/chouon-015-grand-staff.musicxml',
-    './sheets/chouon-031-four-staves.musicxml',
-    './sheets/chouon-031-grand-staff.musicxml',
-    './sheets/chouon-032-four-staves.musicxml',
-    './sheets/chouon-032-grand-staff.musicxml',
-    './sheets/chouon-033-four-staves.musicxml',
-    './sheets/chouon-033-grand-staff.musicxml',
-    './sheets/chouon-034-four-staves.musicxml',
-    './sheets/chouon-034-grand-staff.musicxml',
-    './sheets/chouon-035-four-staves.musicxml',
-    './sheets/chouon-035-grand-staff.musicxml',
-    './sheets/chouon-061-four-staves.musicxml',
-    './sheets/chouon-061-grand-staff.musicxml',
-    './sheets/chouon-062-four-staves.musicxml',
-    './sheets/chouon-062-grand-staff.musicxml',
-    './sheets/chouon-063-four-staves.musicxml',
-    './sheets/chouon-063-grand-staff.musicxml',
-    './sheets/chouon-064-four-staves.musicxml',
-    './sheets/chouon-064-grand-staff.musicxml',
-    './sheets/chouon-065-four-staves.musicxml',
-    './sheets/chouon-065-grand-staff.musicxml',
-  ]
-  const sheetDocumentsPromise: Promise<Map<string, Document>> = (() => {
-    const domParser = new DOMParser()
-    return Promise.all(sheets.map(sheet => {
-      return fetch(sheet).then(res => res.text()).then(data => domParser.parseFromString(data, 'text/xml'))
-    })).then(all => {
-      const map: Map<string, Document> = new Map()
-      sheets.forEach((e, i) => map.set(e, all[i]))
-      return map
-    })
-  })()
 
   const getElementByIdOrError = (elementId: string) => {
     const element = document.getElementById(elementId)
@@ -81,7 +18,8 @@ const init = () => {
   const staffSelector = getElementByIdOrError('staff-selector') as HTMLSelectElement
   const instrumentSelector = getElementByIdOrError('instrument-selector') as HTMLSelectElement
   const transposeInput = getElementByIdOrError('transpose-input') as HTMLButtonElement
-  const playStopButton = document.getElementById('play-stop') as HTMLButtonElement
+  const playButton = document.getElementById('play') as HTMLButtonElement
+  const stopButton = document.getElementById('stop') as HTMLButtonElement
   const showHideButton = document.getElementById('show-hide') as HTMLButtonElement
   const transposeDownButton = getElementByIdOrError('transpose-down') as HTMLButtonElement
   const transposeRandomButton = getElementByIdOrError('transpose-random') as HTMLButtonElement
@@ -91,6 +29,44 @@ const init = () => {
   const autoPlayCheckbox = document.getElementById('auto-play') as HTMLInputElement
 
 
+  const getSheetFileName = (task: string, staff: string) => {
+    return `./sheets/${task}-${staff}.musicxml`
+  }
+  const getMidiFileName = (task: String) => {
+    return `./sheets/${task}.mid`
+  }
+  const tasks = Array.from(taskSelector.options).map(o => o.value)
+  const sheetFileNames = [
+    tasks.map(task => getSheetFileName(task, 'grand-staff')),
+    tasks.map(task => getSheetFileName(task, 'four-staves')),
+  ].flat();
+  const sheetDocumentsPromise: Promise<Map<string, Document>> = (() => {
+    const domParser = new DOMParser()
+    return Promise.all(sheetFileNames.map(sheet => {
+      return fetch(sheet).then(res => res.text()).then(data => domParser.parseFromString(data, 'text/xml'))
+    })).then(all => {
+      const map: Map<string, Document> = new Map()
+      sheetFileNames.forEach((e, i) => map.set(e, all[i]))
+      return map
+    })
+  })()
+  const midiFileNames = tasks.map(getMidiFileName)
+
+  // taskName -> array buffer of midi file
+  var midiFiles: Map<string, ArrayBuffer> | null = null;
+
+  (() => {
+    console.log("midi files loading.")
+    Promise.all(midiFileNames.map(midiFileName => {
+      return fetch(midiFileName).then(res => res.arrayBuffer())
+    })).then(all => {
+      const map: Map<string, ArrayBuffer> = new Map()
+      tasks.forEach((e, i) => map.set(e, all[i]))
+      midiFiles = map
+      console.log("midi files loaded.")
+    })
+  })()
+
   const options = {
     autoResize: true,
     drawTitle: false,
@@ -98,15 +74,11 @@ const init = () => {
   }
   const osmd: OSMD = new OSMD(osmdRenderAreaDiv, options)
 
-  const getFileName = (task: string, staff: string) => {
-    return `./sheets/${task}-${staff}.musicxml`
-  }
-
 
   const buttonDisabledOnLoading = <T>(promise: Promise<T>) => {
-    playStopButton.disabled = true
+    playButton.disabled = true
     return promise.then(p => {
-      playStopButton.disabled = false
+      playButton.disabled = false
       return p
     })
   }
@@ -115,7 +87,7 @@ const init = () => {
   const loadCurrentSelectFile = () => {
     const task = taskSelector.options[taskSelector.selectedIndex].value
     const staff = staffSelector.options[staffSelector.selectedIndex].value
-    const filename = getFileName(task, staff)
+    const filename = getSheetFileName(task, staff)
     return sheetDocumentsPromise.then(sheetDocuments => {
       const doc: Document = sheetDocuments.get(filename)!
       const transposed = transposeMusicXML(doc, parseInt(transposeInput.value), (staff === 'four-staves'))
@@ -133,62 +105,129 @@ const init = () => {
   buttonDisabledOnLoading(loadCurrentSelectFile()).then(() => {})
 
 
-
-
   const audioContext = new AudioContext()
-  audioContext.suspend().then(()=>{})
+  audioContext.suspend().then(() => {})
 
-  // // acoustic_grand_piano, string_ensemble_1
-  var instrument: Player | null = null
-  console.log("soundfont instrument loading.")
-  Soundfont.instrument(audioContext, 'acoustic_grand_piano').then(p => {
-    instrument = p
-    console.log("soundfont instrument loaded.")
-  })
+  var midiPlayer: MidiPlayer.Player | null = null
+
+  const sfNameToUrl = (name: string) => `./soundfonts/${name}-mp3.js`
+  var sfPlayers: Map<string, Soundfont.Player> | null = new Map();
+  var sfPlayer: Soundfont.Player | null = null
+
+  const instrumentNames = Array.from(instrumentSelector.options).map(o => o.value);
+  (() => {
+    console.log("soundfont instruments loading.")
+    Promise.all(instrumentNames.map(instrumentName => {
+      return Soundfont.instrument(audioContext, instrumentName as InstrumentName, {
+        nameToUrl: sfNameToUrl
+      })
+    })).then(all => {
+      const map: Map<string, Soundfont.Player> = new Map()
+      instrumentNames.forEach((e, i) => map.set(e, all[i]))
+      sfPlayers = map
+      sfPlayer = sfPlayers.get(instrumentNames[0])!
+      console.log("soundfont instruments loaded.")
+    })
+  })()
+
+  const changeInstrument = () => {
+    midiStop()
+    const instrumentName = instrumentSelector.options[instrumentSelector.selectedIndex].value
+    if (sfPlayers === null) {
+      console.log("soundfont instruments has not been loaded.")
+      return
+    }
+    sfPlayer = sfPlayers.get(instrumentName)!
+  }
+  instrumentSelector.addEventListener('change', () => changeInstrument())
+
 
   const midiPlay = () => {
-    if (instrument === null) {
+    if (sfPlayer === null) {
       console.log("soundfont instrument has not been loaded.")
       return
     }
+    if (midiFiles === null) {
+      console.log("midi files has not been loaded.")
+      return
+    }
+
+    const transp = parseInt(transposeInput.value)
 
     audioContext.resume().then(() => {})
 
+    const nodesThatMustBeStopped: Map<string, Soundfont.Player> = new Map()
 
-    const player = new MidiPlayer.Player((event: any) => {
-      if (instrument === null) {
+    midiPlayer = new MidiPlayer.Player((event: any) => {
+      if (sfPlayer === null) {
         console.log("soundfont instrument has not been loaded.")
         return
       }
 
-      console.log(event)
-
       if (event.name == 'Note on') {
-        instrument.play(event.noteName, audioContext.currentTime, {gain:event.velocity/100});
-        //document.querySelector('#track-' + event.track + ' code').innerHTML = JSON.stringify(event);
+        const noteId = `${event.channel}-${event.noteNumber}`
+        const node: Soundfont.Player = sfPlayer.start(event.noteNumber + transp, audioContext.currentTime, {gain: event.velocity / 100})
+        nodesThatMustBeStopped.set(noteId, node)
       }
-
-      return
+      if (event.name == 'Note off') {
+        const noteId = `${event.channel}-${event.noteNumber}`
+        const node = nodesThatMustBeStopped.get(noteId)
+        if (node !== undefined) {
+          node.stop()
+          nodesThatMustBeStopped.delete(noteId)
+        }
+      }
+    })
+    midiPlayer.on('endOfFile', () => {
+      if (sfPlayer === null) {
+        console.log("soundfont instrument has not been loaded.")
+        return
+      }
+      sfPlayer.stop()
     })
 
-    player.loadDataUri('data:audio/midi;base64,TVRoZAAAAAYAAQAFA8BNVHJrAAAAHgD/WAQDAhgIAP9ZAgAAAP9RAwknwAD/AwExAf8vAE1UcmsAAAA2AMAwALB5AACwQAAAsFsYALAKEACwB2QAkEpRjwCASgAAkEdLh0CARwAAkEhRlkCASAAB/y8ATVRyawAAADYAwzAAs3kAALNAAACzWxgAswpvALMHXwCTRVGPAINFAACTQ0yHQINDAACTQ1GWQINDAAH/LwBNVHJrAAAANgDBMACxeQAAsUAAALFbGACxCiAAsQdaAJFBUY8AgUEAAJE+S4dAgT4AAJFAUpZAgUAAAf8vAE1UcmsAAAA2AMIwALJ5AACyQAAAslsYALIKXwCyB1oAkjJRjwCCMgAAkjdNh0CCNwAAkjBOlkCCMAAB/y8A')
-    player.play()
-
+    const task = taskSelector.options[taskSelector.selectedIndex].value
+    const midiData = midiFiles.get(task)
+    if (midiData === undefined) {
+      console.log(`midi file of task ${task} not found! something wrong!`)
+    } else {
+      midiPlayer.loadArrayBuffer(midiData)
+      midiPlayer.play()
+    }
 
   }
 
+  function midiStop() {
+    if (sfPlayer === null) {
+      console.log("soundfont instrument has not been loaded.")
+      return
+    }
+
+    sfPlayer.stop()
+
+    if (midiPlayer === null) {
+      console.log("midi player has not been started.")
+      return
+    }
+    midiPlayer.stop()
+  }
+
   const clickEventType = ((window.ontouchstart !== null) ? 'click' : 'touchend');
-  playStopButton.addEventListener(clickEventType, midiPlay)
+  playButton.addEventListener(clickEventType, midiPlay)
+  stopButton.addEventListener(clickEventType, midiStop)
 
 
   const next = () => {
     transposeInput.value = (Math.floor(Math.random() * (6 - (-5) + 1) + (-5))).toString()
     taskSelector.selectedIndex = Math.floor(Math.random() * taskSelector.options.length)
+    midiStop();
     if (autoHideCheckbox.checked) {
       osmdRenderAreaAboveDiv.classList.add("hide-osmd")
     }
     if (autoPlayCheckbox.checked) {
-      return buttonDisabledOnLoading(loadCurrentSelectFile().then(() => {}))
+      return buttonDisabledOnLoading(loadCurrentSelectFile().then(() => {
+        midiPlay();
+      }))
     } else {
       return buttonDisabledOnLoading(loadCurrentSelectFile()).then(() => {})
     }
@@ -207,6 +246,8 @@ const init = () => {
   showHideButton.addEventListener(clickEventType, switchShowHide)
 
   const transpose = (num: number) => {
+    midiStop();
+
     transposeInput.value = num.toString()
     return loadCurrentSelectFile()
   }
@@ -224,27 +265,6 @@ const init = () => {
   transposeDownButton.addEventListener(clickEventType, transposeDown)
   transposeRandomButton.addEventListener(clickEventType, transposeRandom)
   transposeUpButton.addEventListener(clickEventType, transposeUp)
-
-  const keyup = (e: KeyboardEvent) => {
-    if (e.code == 'Space') {
-
-    } else if (e.code == 'Enter') {
-      switchShowHide()
-    } else if (e.code == 'ArrowRight') {
-      next().then(() => {})
-    } else if (e.code == 'ArrowUp') {
-      transposeUp().then(() => {})
-    } else if (e.code == 'ArrowDown') {
-      transposeDown().then(() => {})
-    } else if (e.code == 'KeyT') {
-      transposeRandom().then(() => {})
-    }
-
-
-    return false
-  }
-
-  document.addEventListener('keyup', keyup)
 
 }
 
